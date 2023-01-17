e8af0dacbce9ef8001879f90f5373c27eb225800
Stuntlover <101748104+Stuntlover-TM@users.noreply.github.com>
2023-01-17 17:14:00 +0100

    i am not smart

--- bf_gui.py
+++ bf_gui.py
@@ -85,7 +85,7 @@ def update():
 
     files = requests.get(version_file_url).text.split("\n")
 
-    current_version = "v0.0.1-alph2a"
+    current_version = "v0.0.1-alpha"
 
     version = files[5]
 
