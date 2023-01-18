4c0f18addf9bbcb790e227ddea54d9d8233a4256
Stuntlover <101748104+Stuntlover-TM@users.noreply.github.com>
2023-01-18 15:06:32 +0100

    change version to v0.1.3.2 in bf_gui.py

--- bf_gui.py
+++ bf_gui.py
@@ -75,7 +75,7 @@ class Global:
 
         self.version_file_url = 'https://raw.githubusercontent.com/CodyNinja1/TMIBruteforceGUI/main/bf_gui_version.txt' # This should always stay the same
         self.files = requests.get(self.version_file_url).text.split("\n")
-        self.current_version = "v0.1.3"
+        self.current_version = "v0.1.3.2"
         self.version = self.files[5]
 
     def unpackCoordinates(self):
